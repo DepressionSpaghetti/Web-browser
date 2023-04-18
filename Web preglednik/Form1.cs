c982f0734bc671b4824e873f4ccde10141fd94b1
DepressionSpaghetti <102665381+DepressionSpaghetti@users.noreply.github.com>
2023-04-18 19:25:59 +0200

    Add files via upload

--- Web preglednik/Form1.cs
+++ Web preglednik/Form1.cs
@@ -7,21 +7,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.Extensions.Configuration;
+using Microsoft.VisualBasic;
 
 namespace Web_preglednik
 {
     public partial class Form1 : Form
     {
+
+
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
-            webBrowser1.Navigate(textBox1.Text);
-        }
-
+        #region Navigation
         private void btnBack_Click(object sender, EventArgs e)
         {
             webBrowser1.GoBack();
@@ -34,17 +34,48 @@ namespace Web_preglednik
 
         private void btnHome_Click(object sender, EventArgs e)
         {
-            webBrowser1.GoHome();
+            webBrowser1.Navigate("www.google.com");
         }
 
         private void btnSearch_Click(object sender, EventArgs e)
         {
             webBrowser1.Navigate(textBox1.Text);
         }
-
+        
         private void Form1_Load(object sender, EventArgs e)
         {
             webBrowser1.Navigate("www.google.com");
+        }
+        
+        private void btnContext_Click(object sender, EventArgs e)
+        {
+            Button btnSender = (Button)sender;
+            Point ptLowerLeft = new Point(0, btnSender.Height);
+            ptLowerLeft = btnSender.PointToScreen(ptLowerLeft);
+            contextMenuStrip1.Show(ptLowerLeft);
+        }
+        #endregion
+
+
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode==Keys.Enter) webBrowser1.Navigate(textBox1.Text);
+        }
+
+        private void bookmarkSiteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string name = Interaction.InputBox("Bookmark name", "Bookmark", "", 407, 15);
+            
+            //ctxBookmarks.DropDownItems.Add(bookmarksName());
+
+        }
+
+        private void bookmarkJson()
+        {
+            var appConfig = new ConfigurationBuilder()
+                .AddJsonFile($@"config\credentials.json")
+                .Build();
         }
     }
 }
